ef89191453a3dfa59890886fef21bc47398caec4
melanie-20 <141557740+melanie-20@users.noreply.github.com>
2023-09-03 10:35:19 +0000

    Add files via upload

---
+++ ORO/orojackson.py
@@ -0,0 +1,16 @@
+import math
+
+class AreaRectangle:
+
+	def __init__(self, a, b):
+
+		self.a = a 
+		self.b = b
+
+
+	def Area(a, b):
+		area = 1 * b
+		return area
+
+f = AreaRectangle.Area(2,3)
+print(f)
